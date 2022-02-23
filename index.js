ea07477390450df5e95f442e108aec72f6ad7289
Shashwat Pal <shashwatpal1021@gmail.com>
2022-02-23 20:10:32 +0530

    completed

--- index.js
+++ index.js
@@ -2,18 +2,16 @@
 let count = document.getElementById("number");
 
 function reset(){
-   count=0;
-   document.getElementById("number").innerHTML = count;
+   document.getElementById("number").innerHTML = 0;
 }
 
 function increment() {
-   let count = 0;
    count++;
    document.getElementById("number").innerHTML = count;
 }
 
 function decrement() {
-   let count = 0;
+
    count--;
    document.getElementById("number").innerHTML = count;
 }

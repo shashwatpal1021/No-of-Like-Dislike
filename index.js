9230cc65b700614990f6d6bd541c6abdf9d5a176
Shashwat Pal <shashwatpal1021@gmail.com>
2022-02-23 20:13:01 +0530

    .

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
 let count = document.getElementById("number");
 
-function reset() {
-   count = 0;
-   document.getElementById("number").innerHTML = count;
-}
+// function reset() {
+//    count = 0;
+//    document.getElementById("number").innerHTML = count;
+// }
 
 function increment() {
    count++;

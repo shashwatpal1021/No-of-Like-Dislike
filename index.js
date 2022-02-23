4ab08ae4c8e1a70ac305c066f8026e0b3fec5e99
Shashwat Pal <shashwatpal1021@gmail.com>
2022-02-23 20:42:27 +0530

    Finally completed

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 
-let count = document.getElementById("number");
+let x= document.getElementById("number");
+let count = 0;
 
-// function reset() {
-//    count = 0;
-//    document.getElementById("number").innerHTML = count;
-// }
-
-function increment() {
-   count++;
+function reset() {
+   count = 0;
    document.getElementById("number").innerHTML = count;
 }
 
-function decrement() {
+function increment() {
+   count++;
+   x.innerHTML = count;
+}
 
+function decrement() {
    count--;
-   document.getElementById("number").innerHTML = count;
+   x.innerHTML = count;
 }
 
 
